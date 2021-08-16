f5b0222ead65d6a438be740a8ff62f5308bb7bcc
Yusufbjj <yusuf.92bjj@gmail.com>
2021-08-16 19:30:02 +0100

    Update

--- JS-Fundamentals-May-2021/arraysLab.js/04. Reverse an Array of Strings.js
+++ JS-Fundamentals-May-2021/arraysLab.js/04. Reverse an Array of Strings.js
@@ -1,10 +1,7 @@
 function reverseArrOfStrings(arr){
     let reversedArr= arr.reverse().join(" ");
     console.log(reversedArr);
-<<<<<<< HEAD
-=======
 
     
->>>>>>> 08b8822532a1c20d80af16670a98b74a8ad74732
 }
 reverseArrOfStrings(['a', 'b', 'c', 'd', 'e'])

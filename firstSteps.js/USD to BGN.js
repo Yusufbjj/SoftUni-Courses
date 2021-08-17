82f08fd4c3b727b49998506d2108597132b77a84
Yusufbjj <yusuf.92bjj@gmail.com>
2021-08-17 05:18:05 +0100

    a

--- firstSteps.js/USD to BGN.js
+++
@@ -1,9 +0,0 @@
-function demo(input){
-
-    let usd = Number(input[0]);
-    let bgn = usd * 1.79549;
-    console.log(bgn);
-    
-}
-    
-    demo(["22"])
--- firstSteps.js/radiantstodegrees.js
+++
@@ -1,7 +0,0 @@
-function demo(input){
-
-    let radians = Number(input[0])
-    let degrees = radians * 180 / Math.PI;
-    console.log (degrees.toFixed(0));
-}
-demo(['3.1416'])

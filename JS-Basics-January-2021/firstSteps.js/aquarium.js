c5c2b1e71ef61b1caefc298d772ce52e64b689c8
Yusufbjj <yusuf.92bjj@gmail.com>
2021-08-17 05:16:49 +0100

    New folder

---
+++ JS-Basics-January-2021/firstSteps.js/aquarium.js
@@ -0,0 +1,20 @@
+function demo(input){
+
+    let tankLength = Number(input[0]);
+    let tankWidth = Number(input[1]);
+    let tankHeigth = Number(input[2]);
+    let percentageUsed = Number(input[3]);
+    let tankVolume= tankLength * tankWidth * tankHeigth ; 
+    let tankVolumeLitres = tankVolume * 0.001;
+    let percentageConverted = percentageUsed * 0.01;
+    let litresNeeded = tankVolumeLitres * (1-percentageConverted);
+
+
+
+
+
+    console.log(litresNeeded)
+
+
+}
+demo (['85' ,'75','47','17'])

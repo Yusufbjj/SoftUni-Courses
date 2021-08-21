d337a866b2a54925a7add404995af14561f56938
Yusufbjj <yusuf.92bjj@gmail.com>
2021-08-21 02:51:11 +0100

    Getting familiar with C# syntax

---
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops/04. Back In 30 Minutes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _04._Back_In_30_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = 30 + int.Parse(Console.ReadLine());
+
+            if (minutes > 59)
+            {
+                hours += 1;
+                minutes -= 60;
+            }
+            if (hours > 23)
+            {
+                hours = 0;
+
+            }
+            Console.WriteLine($"{hours}:{minutes:D2}");
+
+        }
+    }
+}

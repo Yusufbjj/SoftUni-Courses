6cb06ac433422469e0ebc85656d8cc4d5a28adc4
Yusufbjj <yusuf.92bjj@gmail.com>
2021-08-24 06:27:04 +0100

    C# Practise

---
+++ C# Fundamentals/Arrays/03. Rounding Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03._Rounding_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nums = "1 2 3 pesho".Split(" ");
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/Arrays/Reading Array Values from a Single Line/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Reading_Array_Values_from_a_Single_Line
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            Console.WriteLine(string.Join("-", arr));
+
+        }
+    }
+}

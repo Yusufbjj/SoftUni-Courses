cde7ac482267f87acad1069dbf62dbe1a59da766
Yusufbjj <yusuf.92bjj@gmail.com>
2021-08-29 15:19:50 +0100

    lists lab

---
+++ C# Fundamentals/Lists/02. Gauss' Trick/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Gauss__Trick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> number = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int times = number.Count;
+            for (int i = 0; i < times / 2; i++)
+            {
+                int first = number[i];
+                int last = number[number.Count - 1];
+                number.RemoveAt(number.Count - 1);
+                number[i] = first + last;
+
+            }
+            Console.WriteLine(string.Join(" ",number));
+        }
+    }
+}

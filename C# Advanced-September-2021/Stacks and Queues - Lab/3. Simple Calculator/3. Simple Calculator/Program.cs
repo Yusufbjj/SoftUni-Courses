f5fa766179b75b3fceb35211615e96769530e655
Yusufbjj <yusuf.92bjj@gmail.com>
2021-09-03 21:44:41 +0100

    stacks and queues lab

---
+++ C# Advanced-September-2021/Stacks and Queues - Lab/3. Simple Calculator/3. Simple Calculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var values = input.Split();
+            var stack = new Stack<string>(values.Reverse());
+
+            while (stack.Count > 1)
+            {
+                int first = int.Parse(stack.Pop());
+                string action = stack.Pop();
+                int second = int.Parse(stack.Pop());
+                if (action == "+")
+                {
+                    stack.Push((first + second).ToString());
+                }
+                else
+                {
+                    stack.Push((first - second).ToString());
+                }
+            }
+            Console.WriteLine(stack.Pop());
+
+
+
+        }
+    }
+}

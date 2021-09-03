d95c4c3050c4611671fe288f9a9fe721d9261009
Yusufbjj <yusuf.92bjj@gmail.com>
2021-09-03 15:27:16 +0100

    stacks and queues lab

--- C# Advanced-September-2021/Stacks and Queues - Lab/2. Stack Sum/2. Stack Sum/Program.cs
+++ C# Advanced-September-2021/Stacks and Queues - Lab/2. Stack Sum/2. Stack Sum/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _2._Stack_Sum
 {
@@ -6,7 +8,42 @@ namespace _2._Stack_Sum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var commandInfo = Console.ReadLine().ToLower();
+            Stack<int> stack = new Stack<int>(input);
+            var sum = 0;
+
+            while (commandInfo != "end")
+            {
+                var tokens = commandInfo.Split();
+                var command = tokens[0].ToLower();
+                if (command == "add")
+                {
+                    int first = int.Parse(tokens[1]);
+                    int second = int.Parse(tokens[2]);
+                    stack.Push(first);
+                    stack.Push(second);
+                }
+                else if (command == "remove")
+                {
+                    int elementsToRemove = int.Parse(tokens[1]);
+                    if (elementsToRemove > stack.Count)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        for (int i = 0; i < elementsToRemove; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+                commandInfo = Console.ReadLine();
+            }
+            sum = stack.Sum();
+            Console.WriteLine($"Sum: {sum}");
+
         }
     }
 }

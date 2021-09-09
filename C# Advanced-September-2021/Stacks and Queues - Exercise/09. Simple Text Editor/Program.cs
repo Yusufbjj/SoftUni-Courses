ce4d9d621bd00c48bee195ef4a098931d5ead7a7
Yusufbjj <yusuf.92bjj@gmail.com>
2021-09-09 23:49:00 +0100

    homework from stack and queues

---
+++ C# Advanced-September-2021/Stacks and Queues - Exercise/09. Simple Text Editor/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09._Simple_Text_Editor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> text = new Stack<string>();
+            string emptyText = "";
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split().ToArray();
+                int comm = int.Parse(command[0]);
+                if (comm == 1)
+                {
+                    text.Push(emptyText);
+                    emptyText += command[1];
+                }
+                else if (comm == 2)
+                {
+                    text.Push(emptyText);
+                    int index = int.Parse(command[1]);
+                    emptyText = text.Peek().Substring(0, emptyText.Length - index);
+                }
+                else if (comm == 3)
+                {
+                    int index = int.Parse(command[1]) - 1;
+                    Console.WriteLine(emptyText[index]);
+                }
+                else if (comm == 4)
+                {
+                    emptyText = text.Pop();
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced-September-2021/Stacks and Queues - Exercise/10. Crossroads/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10._Crossroads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int greenLightSeconds = int.Parse(Console.ReadLine());
+            int freeWindow = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+            Queue<string> queue = new Queue<string>();
+            int totalCarsPassed = 0;
+            char characterHit = ' ';
+            bool isSafe = true;
+            while (command != "END")
+            {
+                int secondsLeft = greenLightSeconds;
+                if (command == "green")
+                {
+                    while (secondsLeft > 0 && queue.Count > 0)
+                    {
+                        string car = queue.Peek();
+                        if (car.Length <= secondsLeft)
+                        {
+                            queue.Dequeue();
+                            secondsLeft = secondsLeft - car.Length;
+                            totalCarsPassed++;
+                        }
+                        else
+                        {
+                            car = car.Substring(secondsLeft);
+                            secondsLeft = 0;
+                            if (car.Length <= freeWindow)
+                            {
+                                queue.Dequeue();
+                                totalCarsPassed++;
+                            }
+                            else
+                            {
+                                Console.WriteLine("A crash happened!");
+                                Console.WriteLine($"{queue.Peek()} was hit at {car[freeWindow]}.");
+                                return;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    string car = command;
+                    queue.Enqueue(car);
+                    secondsLeft = greenLightSeconds;
+
+                }
+
+                command = Console.ReadLine();
+            }
+            if (isSafe)
+            {
+                Console.WriteLine("Everyone is safe.");
+                Console.WriteLine($"{totalCarsPassed} total cars passed the crossroads.");
+            }
+
+        }
+    }
+}

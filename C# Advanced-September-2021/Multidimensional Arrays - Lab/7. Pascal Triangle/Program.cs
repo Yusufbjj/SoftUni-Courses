8893475e7f577909899df8f7de6ea913e24176a9
Yusufbjj <yusuf.92bjj@gmail.com>
2021-09-13 23:24:33 +0100

    jagged arrays

---
+++ C# Advanced-September-2021/Multidimensional Arrays - Lab/7. Pascal Triangle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _7._Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = int.Parse(Console.ReadLine());
+
+            long[][] jagged = new long[height][];
+
+            for (int i = 0; i < height; i++)
+            {
+                long[] row = new long[i + 1];
+                row[0] = 1;
+                row[i] = 1;
+                for (int j = 1; j < i; j++)
+                {
+                    row[j] = jagged[i - 1][j] + jagged[i - 1][j - 1];
+                }
+
+                jagged[i] = row;
+            }
+
+            for (int i = 0; i < height; i++)
+            {
+                Console.WriteLine(string.Join(" ", jagged[i]));
+            }
+        }
+    }
+}

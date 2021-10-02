435232451f3888cce257fb5f56dafd4add4768c0
Yusufbjj <yusuf.92bjj@gmail.com>
2021-10-02 21:05:13 +0100

    Streams, Files and Directories

---
+++ C# Advanced-September-2021/Streams, Files and Directories Lab/5. Slice a File/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace _5._Slice_a_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream streamReader = new FileStream("../../../slice.txt", FileMode.Open))
+            {
+                int chunkSize = (int)streamReader.Length / 4;
+                for (int i = 0; i < 4; i++)
+                {
+                    byte[] buffer = new byte[1];
+                    int count = 0;
+                    using (FileStream streamWriter = new FileStream($"../../../slice-{i + 1}.txt", FileMode.Create, FileAccess.Write))
+                    {
+
+                        while (count < chunkSize)
+                        {
+                            streamReader.Read(buffer, 0, buffer.Length);
+                            streamWriter.Write(buffer, 0, buffer.Length);
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

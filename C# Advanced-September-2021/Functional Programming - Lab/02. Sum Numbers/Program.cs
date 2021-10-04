2f1ea16c4e16460d93abffd314f6dc9dcffde50e
Yusufbjj <yusuf.92bjj@gmail.com>
2021-10-04 17:59:42 +0100

    Functional Programming

---
+++ C# Advanced-September-2021/Functional Programming - Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(", ").Select(int.Parse);
+            Console.WriteLine(numbers.Count());
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}

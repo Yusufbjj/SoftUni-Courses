5697bfee4eadec3f7590f694b00d22fac3d30639
Yusufbjj <yusuf.92bjj@gmail.com>
2021-10-05 06:24:51 +0100

    Functional Programming

---
+++ C# Advanced-September-2021/Functional Programming - Lab/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Count_Uppercase_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = Console.ReadLine().Split(" ",
+                StringSplitOptions.RemoveEmptyEntries);
+            Func<string, bool> upperCaseFirstLetter = word => char.IsUpper(word[0]);
+            var upperLetterWords = words.Where(upperCaseFirstLetter);
+
+            foreach (var word in upperLetterWords)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ C# Advanced-September-2021/Functional Programming - Lab/04. Add VAT/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbersWithVat = Console.ReadLine()
+                .Split(", ")
+                .Select(double.Parse)
+                .Select(x => x * 1.2);
+
+            foreach (var d in numbersWithVat)
+            {
+                Console.WriteLine($"{d:F2}");
+            }
+        }
+    }
+}

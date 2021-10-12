c503028cdda57cb4b0564e12e97f386449c0fed4
Yusufbjj <yusuf.92bjj@gmail.com>
2021-10-12 06:23:26 +0100

    GenericsLab

---
+++ C# Advanced-September-2021/GenericsLab/ArrayCreator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using GenericArrayCreator;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] arrsStrings = ArrayCreator.Create(5, "Pepi");
+            Console.WriteLine(string.Join(" ", arrsStrings));
+            int[] numInts = ArrayCreator.Create(5, 5);
+
+        }
+    }
+}
---
+++ C# Advanced-September-2021/GenericsLab/BoxOfT/BoxOfT.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class BoxOfT<T>
+    {
+        private List<T> elements;
+
+        public BoxOfT()
+        {
+            elements = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            elements.Insert(0, item);
+        }
+
+        public T Remove()
+        {
+            T removedElement = elements[0];
+
+            elements.RemoveAt(0);
+
+            return removedElement;
+        }
+
+        public int Count
+        {
+            get { return elements.Count; }
+        }
+
+    }
+}

61a044ca5c3f5d51779aedddd0dde390010bc58f
Yusufbjj <yusuf.92bjj@gmail.com>
2021-10-14 03:34:23 +0100

    Generics exercise

---
+++ C# Advanced-September-2021/GenericsExercise/GenericBoxOfString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace GenericBoxOfString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<double> strBox = new Box<double>();
+
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+
+                strBox.Add(input);
+            }
+
+            double itemToCompare = double.Parse(Console.ReadLine());
+
+            int result = strBox.CountGreaterThan(itemToCompare);
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
---
+++ C# Advanced-September-2021/GenericsExercise/Tuples/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tuples
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Adam Smith California
+            Mark 2
+            23 21.23212321*/
+            string[] nameTownInput = Console.ReadLine().Split();
+            string name = $"{nameTownInput[0]} {nameTownInput[1]}";
+            string town = nameTownInput[2];
+
+
+            string[] nameBeerInput = Console.ReadLine().Split();
+            string beerName = nameBeerInput[0];
+            int liters = int.Parse(nameBeerInput[1]);
+
+            string[] numbersInput = Console.ReadLine().Split();
+            int integer = int.Parse(numbersInput[0]);
+            double doubleNumber = double.Parse(numbersInput[1]);
+
+
+            MyTuple<string, string> nameTown = new MyTuple<string, string>(name, town);
+
+            MyTuple<string, int> nameBeer = new MyTuple<string, int>(beerName, liters);
+
+            MyTuple<int, double> numbers = new MyTuple<int, double>(integer, doubleNumber);
+
+            Console.WriteLine(nameTown.GetItems());
+            Console.WriteLine(nameBeer.GetItems());
+            Console.WriteLine(numbers.GetItems());
+        }
+    }
+}

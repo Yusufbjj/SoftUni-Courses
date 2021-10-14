73f4be2b9ab427f1d9542178154990470c83d8a5
Yusufbjj <yusuf.92bjj@gmail.com>
2021-10-14 21:52:04 +0100

    FunctionalProgrammingCustomComparator

---
+++ C# Advanced-September-2021/Functional Programming - Exercise/08. Custom Comparator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Custom_Comparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Func<int, int, int> comparator = (num1, num2) => (num1 % 2 == 0 && num2 % 2 != 0) ? -1 :
+                    (num1 % 2 != 0 && num2 % 2 == 0) ? 1 :
+                    num1.CompareTo(num2);
+
+            Array.Sort<int>(numbers,new Comparison<int>(comparator));
+            Console.WriteLine(string.Join(" ",numbers));
+        }
+    }
+}

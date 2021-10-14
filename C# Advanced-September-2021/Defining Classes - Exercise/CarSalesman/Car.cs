800c2c94bebb6c46798b87ad94ef40c53c9e9c5d
Yusufbjj <yusuf.92bjj@gmail.com>
2021-10-14 06:12:17 +0100

    DefiningClassesExercise/CarSalesman/

---
+++ C# Advanced-September-2021/Defining Classes - Exercise/CarSalesman/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace CarSalesman
+{
+    public class Car
+    {
+        public string Model { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public int Weight { get; set; }
+
+        public string Color { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine($"{Model}:");
+
+            stringBuilder.AppendLine(Engine.ToString());
+            
+            stringBuilder.AppendLine($"  Weight: {(Weight > 0 ? Weight.ToString() : "n/a")}");
+            stringBuilder.AppendLine($"  Color: {Color ?? "n/a"}");
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced-September-2021/Defining Classes - Exercise/CarSalesman/Engine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarSalesman
+{
+    public class Engine
+    {
+        public string Model { get; set; }
+
+        public int Power { get; set; }
+
+        public int Displacement { get; set; }
+
+        public string Efficiency { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine($"  {Model}:");
+            stringBuilder.AppendLine($"    Power: {Power}");
+            stringBuilder.AppendLine($"    Displacement: {(Displacement > 0 ? Displacement.ToString() : "n/a")}");
+            stringBuilder.AppendLine($"    Efficiency: {Efficiency ?? "n/a"}");
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}

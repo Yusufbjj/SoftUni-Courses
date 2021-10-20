d8cd43848fc1c347f7b6d62aadb0110d277ba160
Yusufbjj <yusuf.92bjj@gmail.com>
2021-10-20 21:47:54 +0100

    ExamPreparation

---
+++ C# Advanced-September-2021/ExamPreparationExercises/Solutions/FlowerWreaths/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FlowerWreaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> lilies = new Stack<int>(Console.ReadLine().Split(", ").Select(int.Parse));
+            Queue<int> roses = new Queue<int>(Console.ReadLine().Split(", ").Select(int.Parse));
+            int wreaths = 0;
+            int flowers = 0;
+            while (roses.Count > 0 && lilies.Count > 0)
+            {
+                int lili = lilies.Pop();
+                int rose = roses.Dequeue();
+                int sum = lili + rose;
+
+                if (sum == 15)
+                {
+                    wreaths++;
+                }
+                else if (sum < 15)
+                {
+                    flowers += sum;
+                }
+                else
+                {
+                    while (sum > 15)
+                    {
+                        sum -= 2;
+                        if (sum == 15)
+                        {
+                            wreaths++;
+                            break;
+                        }
+                        else if (sum < 15)
+                        {
+                            flowers += sum;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            wreaths += flowers / 15;
+
+            if (wreaths >= 5)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {wreaths} wreaths!");
+            }
+            else
+            {
+                Console.WriteLine($"You didn't make it, you need {5 - wreaths} wreaths more!");
+            }
+        }
+    }
+}

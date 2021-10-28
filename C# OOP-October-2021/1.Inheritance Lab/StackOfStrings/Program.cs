9caaf92b6ea42d1a4462b3a1b549212e3895fb60
Yusufbjj <yusuf.92bjj@gmail.com>
2021-10-28 15:50:53 +0100

    Inheritance

---
+++ C# OOP-October-2021/1.Inheritance Lab/StackOfStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using CustomStack;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+            List<string> list = new List<string>() { "1", "2", "3" };
+
+            stack.AddRange(list);
+        }
+    }
+}
---
+++ C# OOP-October-2021/1.Inheritance Lab/StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        public void AddRange(IEnumerable<string> collection) 
+        {
+            foreach (var ele in collection)
+            {
+                this.Push(ele);
+            }
+        }
+    }
+}

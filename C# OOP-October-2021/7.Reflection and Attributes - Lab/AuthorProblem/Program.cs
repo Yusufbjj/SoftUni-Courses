b3465082de40020c9a082ed5acad84add36ee610
Yusufbjj <yusuf.92bjj@gmail.com>
2021-11-18 00:45:08 +0000

    Reflection and Attributes - Lab

---
+++ C# OOP-October-2021/7.Reflection and Attributes - Lab/AuthorProblem/Program.cs
@@ -0,0 +1,19 @@
+﻿
+namespace AuthorProblem
+{
+    [Author("Az")]
+    [Author("ti")]
+    [Author("tq")]
+    public class StartUp
+    {
+        [Author("Gosho")]
+        public static void Main(string[] args)
+        {
+            var tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+
+        }
+    }
+
+
+}

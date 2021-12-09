541709d869b5f35f3d6cc48a4f9cb640cf3ffec5
Yusufbjj <yusuf.92bjj@gmail.com>
2021-12-09 11:31:07 +0000

    ExamPreparationExercises/C#Advanced

---
+++ C# Advanced-September-2021/ExamPreparationExercises/Solutions/01. Cooking/01. Cooking/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Cooking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> liquids = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            Stack<int> ingredients = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+
+            Dictionary<string, int> foods = new Dictionary<string, int>();
+
+            foods.Add("Bread", 25);
+            foods.Add("Cake", 50);
+            foods.Add("Pastry", 75);
+            foods.Add("Fruit Pie", 100);
+
+            Dictionary<string, int> foodsCooked = new Dictionary<string, int>();
+
+            foodsCooked.Add("Bread", 0);
+            foodsCooked.Add("Cake", 0);
+            foodsCooked.Add("Pastry", 0);
+            foodsCooked.Add("Fruit Pie", 0);
+
+            while (liquids.Count > 0 && ingredients.Count > 0)
+            {
+                int liquid = liquids.Peek();
+                int ingredient = ingredients.Pop();
+
+                int sum = liquid + ingredient;
+
+                if (sum == 25)
+                {
+                    foods.Remove("Bread");
+                    foodsCooked["Bread"]++;
+                }
+                else if (sum == 50)
+                {
+                    foods.Remove("Cake");
+                    foodsCooked["Cake"]++;
+                }
+                else if (sum == 75)
+                {
+                    foods.Remove("Pastry");
+                    foodsCooked["Pastry"]++;
+                }
+                else if (sum == 100)
+                {
+                    foods.Remove("Fruit Pie");
+                    foodsCooked["Fruit Pie"]++;
+                }
+                else
+                {
+                    liquids.Dequeue();
+                    ingredient += 3;
+                    ingredients.Push(ingredient);
+                    continue;
+                }
+
+                liquids.Dequeue();
+                
+            }
+
+            if (foods.Count == 0)
+            {
+                Console.WriteLine("Wohoo! You succeeded in cooking all the food!");
+            }
+            else
+            {
+                Console.WriteLine("Ugh, what a pity! You didn't have enough materials to cook everything.");
+            }
+
+            if (liquids.Any())
+            {
+                Console.WriteLine($"Liquids left: {string.Join(", ", liquids)}");
+            }
+            else
+            {
+                Console.WriteLine("Liquids left: none");
+            }
+
+            if (ingredients.Any())
+            {
+                Console.WriteLine($"Ingredients left: {string.Join(", ", ingredients)}");
+            }
+            else
+            {
+                Console.WriteLine("Ingredients left: none");
+            }
+
+            foreach (var food in foodsCooked.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{food.Key}: {food.Value}");
+            }
+        }
+    }
+}
---
+++ C# Advanced-September-2021/ExamPreparationExercises/Solutions/02. Selling/02. Selling/Program.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Selling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[matrixSize, matrixSize];
+
+            int playerRow = 0;
+            int playerCol = 0;
+
+            int pillarOneRow = 0;
+            int pillarOneCol = 0;
+
+            int pillarTwoRow = 0;
+            int pillarTwoCol = 0;
+
+            double money = 0;
+
+
+            for (int rows = 0; rows < matrixSize; rows++)
+            {
+                string row = Console.ReadLine();
+
+                for (int cols = 0; cols < matrixSize; cols++)
+                {
+                    matrix[rows, cols] = row[cols];
+
+                    if (matrix[rows, cols] == 'S') // player position
+                    {
+                        playerRow = rows;
+                        playerCol = cols;
+
+                    }
+
+                    if (matrix[rows, cols] == 'O') //first pillar position
+                    {
+                        if (pillarOneRow == pillarTwoRow && pillarOneCol == pillarTwoCol)
+                        {
+                            pillarOneRow = rows;
+                            pillarOneCol = cols;
+                        }
+                        else
+                        {
+                            pillarTwoRow = rows;
+                            pillarTwoCol = cols;
+                        }
+                    }
+                }
+            }
+
+
+            while (true)
+            {
+
+                string command = Console.ReadLine();
+
+                matrix[playerRow, playerCol] = '-';
+
+                if (command == "up")
+                {
+                    if (playerRow - 1 >= 0) //valid index 
+                    {
+                        if (matrix[playerRow - 1, playerCol] == 'O')
+                        {
+                            matrix[playerRow - 1, playerCol] = '-';
+                            matrix[pillarTwoRow, pillarTwoCol] = 'S';
+
+                            playerRow = pillarTwoRow;
+                            playerCol = pillarTwoCol;
+
+
+                        }
+                        else if (matrix[playerRow - 1, playerCol] == '-')
+                        {
+                            matrix[playerRow - 1, playerCol] = 'S';
+
+                            playerRow -= 1;
+                        }
+                        else
+                        {
+                            var number = char.GetNumericValue(matrix[playerRow - 1, playerCol]);
+
+                            money += number;
+
+                            matrix[playerRow - 1, playerCol] = 'S';
+
+                            playerRow -= 1;
+                        }
+                    }
+                    else if (playerRow - 1 < 0)
+                    {
+                        Console.WriteLine("Bad news, you are out of the bakery.");
+                        break;
+                    }
+                }
+                else if (command == "down")
+                {
+                    if (playerRow + 1 < matrixSize) //valid index 
+                    {
+                        if (matrix[playerRow + 1, playerCol] == 'O')
+                        {
+                            matrix[playerRow + 1, playerCol] = '-';
+                            matrix[pillarTwoRow, pillarTwoCol] = 'S';
+
+                            playerRow = pillarTwoRow;
+                            playerCol = pillarTwoCol;
+                        }
+                        else if (matrix[playerRow + 1, playerCol] == '-')
+                        {
+                            matrix[playerRow + 1, playerCol] = 'S';
+
+                            playerRow += 1;
+                        }
+                        else
+                        {
+                            var number = char.GetNumericValue(matrix[playerRow + 1, playerCol]);
+
+                            money += number;
+                            matrix[playerRow + 1, playerCol] = 'S';
+
+                            playerRow += 1;
+
+                        }
+                    }
+                    else if (playerRow + 1 >= matrixSize)
+                    {
+                        Console.WriteLine("Bad news, you are out of the bakery.");
+                        break;
+                    }
+                }
+                else if (command == "left")
+                {
+                    if (playerCol - 1 >= 0) //valid index 
+                    {
+                        if (matrix[playerRow, playerCol - 1] == 'O')
+                        {
+                            matrix[playerRow, playerCol - 1] = '-';
+                            matrix[pillarTwoRow, pillarTwoCol] = 'S';
+
+                            playerRow = pillarTwoRow;
+                            playerCol = pillarTwoCol;
+
+
+                        }
+                        else if (matrix[playerRow, playerCol - 1] == '-')
+                        {
+
+                            matrix[playerRow, playerCol - 1] = 'S';
+
+                            playerCol -= 1;
+                        }
+                        else
+                        {
+                            var number = char.GetNumericValue(matrix[playerRow, playerCol-1]);
+
+                            money += number;
+
+                            matrix[playerRow, playerCol - 1] = 'S';
+
+                            playerCol -= 1;
+                        }
+                    }
+                    else if (playerCol - 1 < 0)
+                    {
+                        Console.WriteLine("Bad news, you are out of the bakery.");
+                        break;
+                    }
+                }
+                else if (command == "right")
+                {
+                    if (playerCol + 1 < matrixSize) //valid index 
+                    {
+                        if (matrix[playerRow, playerCol + 1] == 'O')
+                        {
+                            matrix[playerRow, playerCol + 1] = '-';
+                            matrix[pillarTwoRow, pillarTwoCol] = 'S';
+
+                            playerRow = pillarTwoRow;
+                            playerCol = pillarTwoCol;
+
+
+                        }
+                        else if (matrix[playerRow, playerCol + 1] == '-')
+                        {
+
+                            matrix[playerRow, playerCol + 1] = 'S';
+
+                            playerCol += 1;
+                        }
+                        else
+                        {
+                            var number = char.GetNumericValue(matrix[playerRow, playerCol+1]);
+
+                            money += number;
+
+                            matrix[playerRow, playerCol + 1] = 'S';
+
+                            playerCol += 1;
+                        }
+                    }
+                    else if (playerCol + 1 >= matrixSize)
+                    {
+                        Console.WriteLine("Bad news, you are out of the bakery.");
+                        break;
+                    }
+                }
+
+                if (money >= 50)
+                {
+                    Console.WriteLine("Good news! You succeeded in collecting enough money!");
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Money: {money}");
+
+            //print
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    Console.Write(matrix[rows, cols]);
+                }
+
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}

a8c63f68592c231525c771aed2fa9878643642ab
Yusufbjj <yusuf.92bjj@gmail.com>
2022-01-05 18:03:32 +0000

    update

--- JS-Fundamentals-May-2021/finalExam/01. Problem.js
+++ JS-Fundamentals-May-2021/finalExam/01. Problem.js
@@ -1,61 +1,61 @@
 function problem1(input) {
     let email = input.shift();
-    while (input[0]!="Complete") {
+    while (input[0] != "Complete") {
         let line = input.shift();
-        let [command,...others]=line.split(" ");
-        if(command=="Make"){
-            if(others[0]=="Upper"){
-                email=email.toUpperCase();
-            }else if(others[0]=="Lower"){  
-                email=email.toLowerCase();
+        let [command, ...others] = line.split(" ");
+        if (command == "Make") {
+            if (others[0] == "Upper") {
+                email = email.toUpperCase();
+            } else if (others[0] == "Lower") {
+                email = email.toLowerCase();
             }
             console.log(email);
-        }else if(command=="GetDomain"){
+        } else if (command == "GetDomain") {
             let count = Number(others[0]);
-            let substr = email.substring(email.length-count,email.length);
+            let substr = email.substring(email.length - count, email.length);
             console.log(substr);
-        }else if(command=="GetUsername"){
+        } else if (command == "GetUsername") {
             let str = "";
-            if(!email.includes("@")){
+            if (!email.includes("@")) {
                 console.log(`The email ${email} doesn't contain the @ symbol.`);
-            }else{
+            } else {
 
                 for (let ch of email) {
-                    if(ch=="@"){
+                    if (ch == "@") {
                         console.log(`${str}`);
                         break;
                     }
-                    str+=ch;
+                    str += ch;
                 }
             }
 
-        }else if (command=="Replace"){
+        } else if (command == "Replace") {
             let char = others[0];
             let dash = "-";
-            while(email.includes(char)){
-                email=email.replace(char,dash)
+            while (email.includes(char)) {
+                email = email.replace(char, dash)
             }
             console.log(email);
 
-        }else if(command=="Encrypt"){
-            let str ="";
+        } else if (command == "Encrypt") {
+            let str = "";
             for (let ch of email) {
                 let asciChar = ch.charCodeAt(0);
-                str+=asciChar+" ";
+                str += asciChar + " ";
             }
             console.log(str);
         }
-        
+
     }
 }
 problem1(["Mike123@somemail.com",
-"Make Upper",
-"GetDomain 3",
-"GetUsername",
-"Encrypt",
-"Complete"])
+    "Make Upper",
+    "GetDomain 3",
+    "GetUsername",
+    "Encrypt",
+    "Complete"])
 problem1(["AnotherMail.com",
-"Make Lower",
-"GetUsername",
-"Replace a",
-"Complete"])
+    "Make Lower",
+    "GetUsername",
+    "Replace a",
+    "Complete"])

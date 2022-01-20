796c0e120ef662cbebd0c301b496c6fef8755720
Yusufbjj <yusuf.92bjj@gmail.com>
2022-01-20 23:11:02 +0000

    Javascript-Advanced-January-2022/3.Objects and Composition - Lab

---
+++ Javascript-Advanced-January-2022/3.Objects and Composition - Lab/01. City Record.js
@@ -0,0 +1,15 @@
+function cityRecord(city, population, treasury) {
+
+    let record = {
+        name: city,
+        population: population,
+        treasury: treasury
+    };
+
+    return record;
+
+}
+
+cityRecord('Tortuga',
+    7000,
+    15000);

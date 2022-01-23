76bd983eaaaa251b380a252479c21f4d7d1b3e63
Yusufbjj <yusuf.92bjj@gmail.com>
2022-01-23 11:28:49 +0000

    Javascript-Advanced-January-2022/4.DOM Introduction

---
+++ Javascript-Advanced-January-2022/4.DOM Introduction - Lab/01. Edit Element.js
@@ -0,0 +1,9 @@
+function solve(element, match, replacer) {
+
+    while (element.textContent.includes(match)) {
+
+        element.textContent = element.textContent.replace(match, replacer);
+
+    }
+
+}
---
+++ Javascript-Advanced-January-2022/4.DOM Introduction - Lab/02.Collect List Items/CollectListItems.js
@@ -0,0 +1,8 @@
+function extractText() {
+
+    let ulElement = document.getElementById('items');
+
+    let textAreaElement = document.getElementById('result');
+
+    textAreaElement.textContent = ulElement.textContent;
+}

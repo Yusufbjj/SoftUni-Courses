2075f399f3f7c9ae0a01b3e691b9d7489fc994b7
Yusufbjj <yusuf.92bjj@gmail.com>
2022-01-25 08:42:49 +0000

    Javascript-Advanced-January-2022/4.DOM Introduction - Lab

--- Javascript-Advanced-January-2022/4.DOM Introduction - Lab/03.Sum Numbers/SumNumbers.js
+++ Javascript-Advanced-January-2022/4.DOM Introduction - Lab/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,15 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    let firstElement = document.getElementById('num1');
+
+    let secondElement = document.getElementById('num2');
+
+    let firstNum = Number(firstElement.value);
+
+    let secondNum = Number(secondElement.value);
+
+    let sum = firstNum + secondNum;
+
+    let resultEle = document.getElementById('sum');
+
+    resultEle.value = sum;
 }

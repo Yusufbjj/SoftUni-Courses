6c66a17c17be47744168f364d96bf4484cc4861f
Yusufbjj <yusuf.92bjj@gmail.com>
2022-01-28 16:04:11 +0000

    Javascript-Advanced-January-2022/04. JS-Advanced-DOM-Introduction-Exercise

--- Javascript-Advanced-January-2022/04. JS-Advanced-DOM-Introduction-Exercise/03. Accordion/accordion.js
+++ Javascript-Advanced-January-2022/04. JS-Advanced-DOM-Introduction-Exercise/03. Accordion/accordion.js
@@ -1,16 +1,19 @@
 function toggle() {
 
     let button = document.querySelector('.button')
+    let text = document.querySelector('#extra');
 
-    if (button.textContent === 'Less') {
-
-        button.textContent = 'More';
-
-    } else {
+    if (button.textContent === 'More') {
 
         button.textContent = 'Less';
 
-    }
+        text.style.display = 'block';
 
-    console.log(button);
+    } else {
+
+        button.textContent = 'More';
+
+        text.style.display = 'none';
+
+    }
 }
--- Javascript-Advanced-January-2022/04. JS-Advanced-DOM-Introduction-Exercise/04. Search in List/search.js
+++ Javascript-Advanced-January-2022/04. JS-Advanced-DOM-Introduction-Exercise/04. Search in List/search.js
@@ -1,3 +1,32 @@
 function search() {
-   // TODO
+
+   let userWord = document.getElementById('searchText').value;
+
+   let townElement = Array.from(document.querySelectorAll('#towns li'));
+
+   let match = document.getElementById('result');
+
+   let count = 0;
+
+   for (const town of townElement) {
+
+      if (town.textContent.includes(userWord)) {
+
+         town.style.fontWeight = 'bold';
+
+         town.style.textDecoration = 'underline';
+
+         count++;
+
+      } else {
+         
+         town.style.fontWeight = 'normal';
+
+         town.style.textDecoration = 'none';
+      }
+
+   }
+
+   match.textContent = `${count} matches found`;
+
 }

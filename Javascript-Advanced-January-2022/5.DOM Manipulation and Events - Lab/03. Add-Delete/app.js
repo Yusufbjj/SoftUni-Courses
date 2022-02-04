b28f0d8a87670402914eba268a4f0c200c5d1d57
Yusufbjj <yusuf.92bjj@gmail.com>
2022-02-04 17:50:26 +0000

    Javascript-Advanced-January-2022/5.DOM Manipulation and Events - Lab

--- Javascript-Advanced-January-2022/5.DOM Manipulation and Events - Lab/03. Add-Delete/app.js
+++ Javascript-Advanced-January-2022/5.DOM Manipulation and Events - Lab/03. Add-Delete/app.js
@@ -1,3 +1,21 @@
 function addItem() {
-    //TODO...
+    let itemsElement = document.getElementById('items');
+    let inputElement = document.getElementById('newItemText');
+
+    let liElement = document.createElement('li');
+    liElement.textContent = inputElement.value;
+
+    inputElement.value = '';
+
+    let deleteElement = document.createElement('a');
+    deleteElement.href = '#';
+    deleteElement.textContent = '[Delete]';
+    deleteElement.addEventListener('click', (e) => {
+       e.currentTarget.parentElement.remove();
+    });
+
+
+    liElement.appendChild(deleteElement);
+
+    itemsElement.appendChild(liElement);
 }

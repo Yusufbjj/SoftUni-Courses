857e6fbe79c66274b2dcc42eb8e3acbfbd3e6c47
Yusufbjj <yusuf.92bjj@gmail.com>
2022-02-15 21:44:27 +0000

    Javascript-Advanced-January-2022/7.Unit Testing  Exercise

--- Javascript-Advanced-January-2022/7.Unit Testing  Exercise/index.test.js
+++ Javascript-Advanced-January-2022/7.Unit Testing  Exercise/index.test.js
@@ -2,23 +2,120 @@
 
 // const lookupChar = require('./03. Char Lookup.js');
 
+const mathEnforcer = require('./04. Math Enforcer.js');
 
 const { assert } = require('chai');
 
+describe('mathEnforcer function tests', () => {
+    describe('add five function tests', () => {
+        it('should return undefined with string', () => {
+            assert.equal(mathEnforcer.addFive('TEST'), undefined);
+        });
+
+        it('should return undefined with an array', () => {
+            assert.equal(mathEnforcer.addFive([]), undefined);
+        });
+
+        it('should return undefined with an object', () => {
+            assert.equal(mathEnforcer.addFive({}), undefined);
+        });
+
+        it('should return undefined with undefined', () => {
+            assert.equal(mathEnforcer.addFive(undefined), undefined);
+        });
+
+        it('should return undefined with null', () => {
+            assert.equal(mathEnforcer.addFive(null), undefined);
+        });
+
+        it('positive number 5', () => {
+            assert.equal(mathEnforcer.addFive(5), 10);
+        });
+
+        it('negative number -5', () => {
+            assert.equal(mathEnforcer.addFive(-5), 0);
+        });
+
+        it('decimal number +5.5', () => {
+            assert.equal(mathEnforcer.addFive(5.5), 10.5);
+        });
+
+    });
+
+    describe('subtract ten function tests', () => {
+        it('should return undefined with string', () => {
+            assert.equal(mathEnforcer.subtractTen('TEST'), undefined);
+        });
+
+        it('should return undefined with an array', () => {
+            assert.equal(mathEnforcer.subtractTen([]), undefined);
+        });
+
+        it('should return undefined with an object', () => {
+            assert.equal(mathEnforcer.subtractTen({}), undefined);
+        });
+
+        it('should return undefined with undefined', () => {
+            assert.equal(mathEnforcer.subtractTen(undefined), undefined);
+        });
+
+        it('should return undefined with null', () => {
+            assert.equal(mathEnforcer.subtractTen(null), undefined);
+        });
+
+        it('positive integer number 5', () => {
+            assert.equal(mathEnforcer.subtractTen(5), -5);
+        });
+
+        it('negative integer number -5', () => {
+            assert.equal(mathEnforcer.subtractTen(-5), -15);
+        });
+
+        it('decimal number 10.5', () => {
+            assert.equal(mathEnforcer.subtractTen(10.5), 0.5);
+        });
+    });
+
+    describe('sum of two numbers function tests', () => {
+        it('two positive integer numbers', () => {
+            assert.equal(mathEnforcer.sum(10, 20), 30);
+        });
+
+        it('two negative numbers', () => {
+            assert.equal(mathEnforcer.sum(-10, -5), -15);
+        });
+
+        it('two decimal numbers', () => {
+            assert.equal(mathEnforcer.sum(10.2, 5.5), 15.7);
+        });
+
+        it('first parameter string,second number', () => {
+            assert.equal(mathEnforcer.sum('gfdgdf', 5), undefined);
+        });
+
+        it('first parameter number,second string', () => {
+            assert.equal(mathEnforcer.sum(5, 'dgfd'), undefined);
+        });
+    });
+});
+
+
+
+
 // describe('isOddOrEven function test', () => {
-//     it('test with num', () => {
+//     it('test with num as parameter', () => {
 //         assert.equal(isOddOrEven(5),undefined);
 //     });
 
-//     it('test with arr', () => {
+//     it('test with arr parameter', () => {
 //         assert.equal(isOddOrEven([]),undefined);
 //     });
 
-//     it('test with even str', () => {
+//     it('test with even length string', () => {
 //         assert.equal(isOddOrEven("Hi"),"even");
 //     });
 
-//     it('test with even str', () => {
+//     it('test with odd length string', () => {
 //         assert.equal(isOddOrEven("Hello"),"odd");
 //     });
 

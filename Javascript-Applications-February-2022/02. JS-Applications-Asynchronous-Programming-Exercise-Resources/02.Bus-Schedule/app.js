33f064cdb2a137c8cd50df76c185309d9a5633d2
Yusufbjj <yusuf.92bjj@gmail.com>
2022-03-18 00:39:50 +0000

    Javascript-Applications-February-2022/02. JS-Applications-Asynchronous-Programming-Exercise

--- Javascript-Applications-February-2022/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/02.Bus-Schedule/app.js
+++ Javascript-Applications-February-2022/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/02.Bus-Schedule/app.js
@@ -1,11 +1,36 @@
 function solve() {
 
-    function depart() {
-        console.log('Depart TODO...');
+    const label = document.querySelector('#info span');
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+
+    let stop = {
+        next: 'depot'
+    };
+
+    async function depart() {
+        //get info for next stop
+        //display next stop 
+
+        departBtn.disabled = true;
+        
+        const url = `http://localhost:3030/jsonstore/bus/schedule/${stop.next}`;
+
+        const res = await fetch(url);
+
+        stop = await res.json();
+
+        label.textContent = `Next stop ${stop.name}`
+
+        arriveBtn.disabled = false;
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        arriveBtn.disabled = true;
+
+        departBtn.disabled = false;
+
+        label.textContent = `Arriving at ${stop.name}`
     }
 
     return {
--- Javascript-Applications-February-2022/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/05. Accordion/accordion.js
+++ Javascript-Applications-February-2022/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/05. Accordion/accordion.js
@@ -1,3 +1,82 @@
-function solution() {
-    //TODO .....
+async function solution() {
+
+    const main = document.getElementById('main');
+
+    const url = `http://localhost:3030/jsonstore/advanced/articles/list`;
+
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    data.forEach(element => {
+        let divAccordion = createElement('div', '', ['class', 'accordion']);
+
+        let divHead = createElement('div', '', ['class', 'head']);
+
+        let span = createElement('span', element.title);
+
+        let button = createElement('button', 'More', ['class', 'button', 'id', element._id]);
+
+        let divExtra = createElement('div', '', ['class', 'extra']);
+
+        let p = createElement('p');
+
+        button.addEventListener('click', toggle);
+
+        main.appendChild(divAccordion);
+        divAccordion.appendChild(divHead);
+        divAccordion.appendChild(divExtra);
+        divHead.appendChild(span);
+        divHead.appendChild(button);
+        
+        divExtra.appendChild(p);
+        
+
+    });
+
+    async function toggle(ev) {
+
+        const accordion = ev.target.parentNode.parentNode;
+
+        const p = ev.target.parentNode.parentNode.children[1].children[0];
+        console.log(p);
+
+        const extra = ev.target.parentNode.parentNode.children[1];
+
+        const id = ev.target.id;
+        const url = `http://localhost:3030/jsonstore/advanced/articles/details/${id}`;
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        p.textContent = data.content;
+
+        const hidden = ev.target.textContent === 'More';
+
+        extra.style.display = hidden ? 'block' : 'none';
+        ev.target.textContent = hidden ? 'Less' : 'More';
+
+    }
+
+    function createElement(type, content, attributes = []) {
+
+        const element = document.createElement(type);
+
+        if (content) {
+            element.textContent = content;
+        }
+
+        if (attributes.length > 0) {
+
+            for (let index = 0; index < attributes.length; index += 2) {
+
+                element.setAttribute(attributes[index], attributes[index + 1])
+
+            }
+        }
+
+        return element;
+    }
+
 }
+solution();
